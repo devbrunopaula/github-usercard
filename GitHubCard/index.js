826daf23d89ba7ca503e7df2af2a38ce819867d6
Bruno Paula <brunopaula@dryserv.com>
2020-06-11 19:15:21 -0400

    added 1 card and  fetched the data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,23 @@
     https://api.github.com/users/<your name>
 */
 
+
+
+function getData() {
+axios({
+    method: 'get',
+    url: 'https://api.github.com/users/devbrunopaula'
+  })
+  .then( res =>  Card(res.data))
+  .catch( err => console.log(err))
+
+}
+
+getData()
+ 
+
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -11,6 +28,46 @@
 
     Skip to STEP 3.
 */
+{/* <img src="${avatar_url}" alt="${name}">
+<div>
+ <h1>${name}</h1>
+ <h3>${login}</h3>
+ <p>location: ${location}</p>
+ <p>Profile: <a href="${html_url}">GitHub Link</a></p>
+ <p>Followers: ${followers}</p>
+ <p>Bio:${bio}</p>
+</div> */}
+// const CardComp =  document.querySelector('.cards').innerHTML = Card()
+const body = document.querySelector('body')
+const CardComp =  document.querySelector('.cards')
+
+function Card(data) {
+console.log(data)
+  const { avatar_url, name,login, location, followers, html_url, bio } = data 
+  const CardComp =  document.querySelector('.cards__container')
+
+  CardComp.innerHTML += `
+            <div class="card">
+            <img src="${avatar_url}" alt="${name}">
+              <div>
+                <h1> ${name}</h1>
+                <h3> ${login}</h3>
+                <p>Location: ${location}</p>
+                <p>Profile: <a href="${html_url}">GitHub Link</a></p>
+                <p>Followers: ${followers}</p>
+                <p>Bio: ${bio}</p>
+              </div>
+            </div> 
+            `
+
+ return CardComp
+
+}
+
+
+
+body.appendChild = Card()
+
 
 /*
   STEP 4: Pass the data received from Github into your function,
@@ -58,3 +115,34 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
+// const cardImage = document.createElement('img')
+// const h1 = document.createElement('h1')
+// const h3 = document.createElement('h3')
+// const locattionP = document.createElement('p')
+// const profileP = document.createElement('p')
+// const profileLink = document.createElement('a')
+// const followersP = document.createElement('p')
+
+// // Attri
+//  cardImage.src = avatar_url
+//  profileLink.href = html_url
+
+// //  Text Content
+// h1.textContent = name
+// h3.textContent = login
+// locattionP.textContent = `location: ${location}` 
+// profileP.textContent = 'Profile'
+// followersP.textContent = followers
+
+// // append
+//  CardComp.appendChild(cardImage)
+//  CardComp.appendChild(h1)
+//  CardComp.appendChild(locattionP)
+//  CardComp.appendChild(profileP)
+//  profileP.appendChild(profileLink)
+//  CardComp.appendChild(followersP)
+ 
+
+//  return CardComp
